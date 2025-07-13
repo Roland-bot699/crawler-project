89f2c27a6135b48b8f7895ad19429260fc38ebc3
許佑全 <xuyouquan@xuyouquandeMacBook-Air.local>
2025-07-13 21:53:31 +0800

    chore: upgrade to puppeteer v24 and node >=20.18.1

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,10 @@
-
 const express = require("express");
 const router = express.Router();
 const runPuppeteer = require("../puppeteer");
 
 router.post("/", async (req, res) => {
   const { url } = req.body;
+
   try {
     const result = await runPuppeteer(url);
     res.json(result);
